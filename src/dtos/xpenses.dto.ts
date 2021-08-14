50f30fafd940b350cfaff7b82d884bc3e049f0d7
Michiel Leunens <michiel.leunens@gmail.com>
2021-08-14 17:27:56 +0200

    Add Xpense model

---
+++ src/dtos/xpenses.dto.ts
@@ -0,0 +1,18 @@
+import {  IsDate, IsNumber, IsString, IsUrl } from 'class-validator';
+
+export class CreateXpenseDto {
+  @IsString()
+  public title: string;
+
+  @IsUrl()
+  public imageUrl: string;
+
+  @IsDate()
+  public date: Date;
+
+  @IsNumber()
+  public cost: number;
+
+  @IsString()
+  public category: string;
+}
---
+++ src/models/xpenses.model.ts
@@ -0,0 +1,29 @@
+import { model, Schema, Document } from 'mongoose';
+import { Xpense } from '@interfaces/xpenses.interface';
+
+const xpenseSchema: Schema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+  },
+  date: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+  cost: {
+    type: Number,
+    required: true,
+    min: 0
+  },
+  category: {
+    type: String,
+  },
+});
+
+const xpenseModel = model<Xpense & Document>('Xpense', xpenseSchema);
+
+export default xpenseModel;
